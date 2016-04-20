f41df7c319feec1dced2bc87bdf5d97f13ecc5da
wjp <wangjp@mittop.com>
2016-04-20 09:50:27 +0800

    up

--- src/main/java/com/enn/test/webCashTest.java
+++ src/main/java/com/enn/test/webCashTest.java
@@ -21,7 +21,64 @@ public class webCashTest {
         json.put("id","123456");
         String body = json.toString();
         HttpPostUtil HttpPostUtil = new HttpPostUtil();
+        String url = "http://localhost:8080/webCash/custom/app/setting";
+        HttpPostUtil.sendJSON(url, body);
+    }
+
+
+    /**
+     * 调起收银台
+     */
+    @Test
+    public void test2() throws IOException, NoSuchAlgorithmException {
+        JSONObject json = new JSONObject();
+        json.put("id","123456");
+        String body = json.toString();
+        HttpPostUtil HttpPostUtil = new HttpPostUtil();
         String url = "http://localhost:8080/webCash/pay/app/unifiedOrder";
+        HttpPostUtil.sendJSON(url, body);
+    }
+
+    /**
+     * 设置成功后端回调
+     */
+    @Test
+    public void test3() throws IOException, NoSuchAlgorithmException {
+        JSONObject json = new JSONObject();
+        json.put("thr_user_no","90090");
+        json.put("result", "SUCCESS");
+        json.put("thr_login_no","liuzw");
+        String body = json.toString();
+        HttpPostUtil HttpPostUtil = new HttpPostUtil();
+        String url = "http://10.37.149.92:8351/3rd/pay/notifyBandResult";
+        HttpPostUtil.sendJSON(url, body);
+    }
+
+    /**
+     * 信息查询
+     */
+    @Test
+    public void test4() throws IOException, NoSuchAlgorithmException {
+        JSONObject json = new JSONObject();
+        json.put("id","123456");
+        String body = json.toString();
+        HttpPostUtil HttpPostUtil = new HttpPostUtil();
+        String url = "http://localhost:8080/webCash/custom/app/info";
+        HttpPostUtil.sendJSON(url, body);
+    }
+
+    /**
+     * 支付成功后端回调
+     */
+    @Test
+    public void test5() throws IOException, NoSuchAlgorithmException {
+        JSONObject json = new JSONObject();
+        json.put("thr_user_no","90090");
+        json.put("result", "SUCCESS");
+        json.put("thr_login_no","liuzw");
+        String body = json.toString();
+        HttpPostUtil HttpPostUtil = new HttpPostUtil();
+        String url = "http://10.37.149.92:8351/3rd/pay/notifyBandResult";
         HttpPostUtil.sendJSON(url, body);
     }
 
