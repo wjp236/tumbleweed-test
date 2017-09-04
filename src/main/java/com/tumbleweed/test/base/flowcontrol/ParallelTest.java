9f0aa77842f472012e9e56d2372e7d9251d58981
wangjianping <wangjpb@ennew.cn>
2017-09-04 15:48:58 +0800

    提交

---
+++ src/main/java/com/tumbleweed/test/base/flowcontrol/ParallelTest.java
@@ -0,0 +1,58 @@
+package com.tumbleweed.test.base.flowcontrol;
+
+import org.junit.Test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * 描述:并发控制
+ *
+ * @author: mylover
+ * @Time: 30/08/2017.
+ */
+public class ParallelTest {
+
+    @Test
+    public void test1() {
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
+        executorService.submit(() -> handler());
+    }
+
+    private static void handler() {
+        boolean acquire = acquire();
+
+        try {
+            if (acquire) {
+                System.out.println("业务执行中");
+            } else {
+                System.out.println("我被拒绝了");
+            }
+        } finally {
+            release();
+        }
+    }
+
+    static final Semaphore semaphore = new Semaphore(10);
+
+    /**
+     * 获取
+     * @return
+     */
+    public static boolean acquire() {
+        return semaphore.tryAcquire();
+    }
+
+    /**
+     * 释放
+     * @return
+     */
+    public static void release() {
+        semaphore.release();
+    }
+
+
+
+
+}
